4dbf2a00f1bdf145d85354b74747739a2bff72cc
98Chanaka <chanud1998@gmail.com>
2025-06-25 17:34:34 +0530

    add to bulk upload

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -194,4 +194,6 @@ class ItemController extends Controller
             ], 500);
         }
     }
+
+   
 }
--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ Route::post('/items', [ItemController::class, 'store'])->name('items.store');
 Route::get('/items/{id}/edit', [ItemController::class, 'edit'])->name('items.edit');
 Route::put('/items/{id}', [ItemController::class, 'update'])->name('items.update');
 Route::delete('/items/{id}', [ItemController::class, 'destroy'])->name('items.destroy');
+Route::post('/items/bulk-upload', [ItemController::class, 'bulkUpload'])->name('items.bulk-upload');
 
 
 Route::get('/pos-dashboard', [PosController::class, 'index'])->name('pos.index');
